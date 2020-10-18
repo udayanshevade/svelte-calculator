a6959afe2095c698c48d6b63a796038128ea8867
Udayan Shevade <udayan.shevade@gmail.com>
2020-10-18 01:09:29 -0700

    feat: add buttons config, add tests

--- src/containers/Calculator/Calculator.spec.js
+++ src/containers/Calculator/Calculator.spec.js
@@ -1,10 +1,22 @@
 import { render, screen } from '@testing-library/svelte';
 import userEvent from '@testing-library/user-event';
-import Calculator from './Calculator.svelte';
+import Calculator, { config } from './Calculator.svelte';
 
 describe('Calculator', () => {
   it('renders correctly', () => {
     render(Calculator);
-    expect(screen.getByRole('button')).toBeInTheDocument();
+
+    const buttonsData = config.flatMap(rowConfig => rowConfig);
+
+    const buttons = screen.getAllByRole('button');
+
+    expect(buttons).toHaveLength(buttonsData.length);
+
+    buttons.forEach((button, i) => {
+      const { text, title, value } = buttonsData[i];
+      expect(button).toHaveTextContent(text);
+      expect(button).toHaveAttribute('title', title);
+      expect(button).toHaveValue(String(value));
+    });
   });
 });
--- src/containers/Calculator/Calculator.svelte
+++ src/containers/Calculator/Calculator.svelte
@@ -1,14 +1,141 @@
 <script lang="ts">
   import Button from '../../components/Button/Button.svelte';
-  let onClick = () => {};
+
+  const handleButtonClick = (buttonType: number | string) => {
+    // TODO: handle button click
+    console.log(buttonType);
+  };
 </script>
 
-<section>
-  <Button
-    id="equals-button"
-    title="equals"
-    className="button button--equals"
-    text="="
-    onClick={onClick}
-  />
+<section class="calculator">
+  {#each config as rowConfig}
+    <div class="row">
+      {#each rowConfig as { id, value, title, className, text } (id)}
+        <Button
+          id={id}
+          title={title}
+          className={`button ${className}`}
+          text={text}
+          value={value}
+          onClick={() => handleButtonClick(value)}
+        />
+      {/each}
+    </div>
+  {/each}
 </section>
+
+<script lang="ts" context="module">
+  export const config: {
+    id: string,
+    title: string,
+    text: string,
+    value: number | string,
+    className?: string
+  }[][] = [
+    [
+      {
+        id: 'seven',
+        title: 'Seven',
+        text: '7',
+        value: 7
+      },
+      {
+        id: 'eight',
+        title: 'Eight',
+        text: '8',
+        value: 8
+      },
+      {
+        id: 'nine',
+        title: 'Nine',
+        text: '9',
+        value: 9
+      },
+      {
+        id: 'divide',
+        title: 'Divide',
+        text: '/',
+        value: '/'
+      }
+    ],
+    [
+      {
+        id: 'four',
+        title: 'Four',
+        text: '4',
+        value: 4
+      },
+      {
+        id: 'five',
+        title: 'Five',
+        text: '5',
+        value: 5
+      },
+      {
+        id: 'six',
+        title: 'Six',
+        text: '6',
+        value: 6
+      },
+      {
+        id: 'multiply',
+        title: 'Multiply',
+        text: '*',
+        value: '*'
+      }
+    ],
+    [
+      {
+        id: 'one',
+        title: 'One',
+        text: '1',
+        value: 1
+      },
+      {
+        id: 'two',
+        title: 'Two',
+        text: '2',
+        value: 2
+      },
+      {
+        id: 'three',
+        title: 'Three',
+        text: '3',
+        value: 3
+      },
+      {
+        id: 'subtract',
+        title: 'Subtract',
+        text: '-',
+        value: '-'
+      }
+    ],
+    [
+      {
+        id: 'zero',
+        title: 'Zero',
+        text: '0',
+        value: 0
+      },
+      {
+        id: 'decimal',
+        title: 'Decimal',
+        text: '.',
+        value: '.'
+      },
+      {
+        id: 'equals',
+        title: 'Equals',
+        className: 'button--equals',
+        text: '=',
+        value: '='
+      },
+      {
+        id: 'add',
+        title: 'Add',
+        text: '+',
+        value: '+'
+      }
+    ]
+  ];
+</script>
