dc32513ce2d20995d008562eb7431996b7fcfca5
Udayan Shevade <udayan.shevade@gmail.com>
2020-10-18 00:31:18 -0700

    feat: add button component, tests

---
+++ src/components/Button/Button.spec.js
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
+import Button from './Button.svelte';
+
+const mockProps = {
+  id: 'submit-btn',
+  className: 'btn',
+  title: 'submit',
+  value: 'foo',
+  text: 'submit',
+  onClick: jest.fn()
+};
+
+describe('Button', () => {
+  beforeEach(() => {
+    mockProps.onClick.mockClear();
+  });
+
+  it('renders correctly', () => {
+    const { id, title, className, text, value } = mockProps;
+    render(Button, mockProps);
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+
+    expect(button).toHaveAttribute('id', id);
+    expect(button).toHaveAttribute('title', title);
+    expect(button).toHaveClass(className);
+    expect(button).toHaveValue(value);
+    expect(button).toHaveTextContent(text);
+  });
+
+  it('reacts to being clicked', () => {
+    const { onClick } = mockProps;
+    render(Button, mockProps);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    expect(onClick).toHaveBeenCalled();
+  });
+});
---
+++ src/components/Button/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let text: string;
+  export let id: string;
+  export let className: string;
+  export let title: string;
+  export let value: string | number = null;
+  export let onClick: (e: MouseEvent) => void | Promise<void>;
+</script>
+
+<button
+  id={id}
+  class={className}
+  title={title}
+  value={value}
+  on:click={onClick}
+>
+  {text}
+</button>
